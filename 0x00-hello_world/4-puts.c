ffb57f5b4125019bc4176bdf59cb43645de9068c
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-16 15:47:52 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-int puts(Programming is like building a multilingual puzzle) 
+#include <stdio.h>
+
+/* The code prints out "Processing is like a multilingual puzzle" */
+
+int main(void)
+{
+	puts('\'Programming is like a multilingual puzzle''\n')
+	return (0)
+}	
