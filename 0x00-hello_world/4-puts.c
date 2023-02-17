77c0d0fa91a035c2a020a602a955ab9bcc3998c3
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-16 16:48:58 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 
 /**
  *  main - entry
- *  return 0
+ *  return (0)
  */
 
 int main(void)
 
 {
-	puts('\'Programming is like building a multilingual puzzle');
+puts("\"Programming is like building a multilingual puzzle");
 	
-	return (0)
+return (0)
 }	
