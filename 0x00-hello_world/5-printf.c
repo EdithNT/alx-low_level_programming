8daba9da1799a47f4161d11d69063925aef66af1
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-16 17:26:57 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: (0) always
+ */
+
+int main(void)
+
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+
+	return (0);
+
+}
