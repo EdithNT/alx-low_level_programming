0457f6084c6dbc9df9cbfb5c8779b6f64d7928a6
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-16 16:27:09 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 
-/* main - prints "Programming is like building a multilingual puzzle" */
+/**
+ *  main - prints "Programming is like building a multilingual puzzle"
+ *  return 0;
+ */
 
 int main(void)
+
 {
-	puts('\'Programming is like a multilingual puzzle')
-	return (0)
+	puts('\'Programming is like a multilingual puzzle');
+	
+	return 0
 }	
