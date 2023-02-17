2761f9678d01eb460a110309db016ceb2a86a595
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-16 21:08:20 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - entry point for working
+ * main - entry point for workinig
  * Return: (0) always
  */
 
@@ -11,7 +11,7 @@ int main(void)
 	printf("size of a char: %zu byte(s)\n", sizeof(char));
 
 	printf("size of an int: %zu byte(s)\n", sizeof(int));
-	printf("size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("size of a float: %zu byte(s)\n", sizeof(float));
 
