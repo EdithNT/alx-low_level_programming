68f59acfa0c1eefea381c20d1fae718068e2cbd2
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-17 18:34:41 -0800

    numbs

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -print single digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num = <= 9; num++)
+	{
+	putchar(num + 0);
+	}
+	putchar('\n';)
+
+	return (0);
+}
