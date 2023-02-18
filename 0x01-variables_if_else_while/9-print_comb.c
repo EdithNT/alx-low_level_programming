6fd35d1fdece89fbf1314725913a2b508603ce5b
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-17 20:43:41 -0800

    coomboo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int num1;
 
-	for (n = 0; n < 10; n++)
+	for (num1 = 0; num1 < 10; num1++)
 	{
-	putchar(n + '0');
-	if (n < 9)
+	putchar(num1 + '0');
+	if (num1 < 9)
 	{
 	putchar(',');
 	putchar(' ');
