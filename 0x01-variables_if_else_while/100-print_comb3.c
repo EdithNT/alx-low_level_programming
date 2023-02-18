0cb71a46f9f2d402dcd22dcf7d3291480564aaed
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-17 19:53:37 -0800

    extra

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible ways of combinations
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int a, b, c;
+
+	for (a = 0; i < 100; a++)
+	{
+	b = a / 10;
+	c = a % 10;
+	if (b < c)
+	{
+	putchar(b + '0');
+	putchar(c + '0');
+	if (a < 89)
+	{
+	putchar(44);
+	putchar(32);
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
