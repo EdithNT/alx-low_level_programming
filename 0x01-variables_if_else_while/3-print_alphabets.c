1f46b62ea7eec9156741f88d475c72f2fec27eea
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-17 17:13:19 -0800

    mix

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+	for (alphabet = 'A''; alphabet <= 'Z'; alphabet++)
+	{
+	putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
