a163927b49b1c7824073f699699cba4d70a7d4bd
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-22 09:15:09 -0800

    search for lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,15 @@
 /**
  * _islower - checks for lower case characters
  *
- * Return: 1 if c is lowercase
- * Return: 0
+ * @c: the character to print
+ *
+ * Return: 1 if c is lowercase, or 0 otherwise
  */
 
 int _islower(int c)
 {
-	char c;
-
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		_putchar("'%c' is lowercase);
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar("'%c' is lowercase");
 	return (0);
 	else
 		return (0);
