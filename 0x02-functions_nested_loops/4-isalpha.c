54f07b3269c0becbe0c0a78c2b01dc6f27870b86
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-22 10:13:05 -0800

    search for all alphabets in both cases

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - we are checking for alphabets, all cases
+ *
+ * @c: value that will be checked
+ *
+ * Return: 1 if it is a letter, whether lower or uppercase; 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a', 'A' && c <= 'z', 'Z')
+		return (1);
+	else
+		return (0);
+}
