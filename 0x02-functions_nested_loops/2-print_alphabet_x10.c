1b47be3e8909515d976cc42c7d4f36fad93c0c33
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-21 19:54:33 -0800

    alphax10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabetx10;
+
+	for (alphabetx10=0; alphabetx10 <= 10; alphabetx10++)
+		_putchar(alphabetx10)
