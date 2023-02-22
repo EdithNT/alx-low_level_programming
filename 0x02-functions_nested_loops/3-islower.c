3f7826b2ff245746a8af133a10f10232bc0a592e
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-22 09:04:37 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case characters
+ *
+ * Return: 1 if c is lowercase
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	char c;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar("'%c' is lowercase);
+	return (0);
+	else
+		return (0);
+}
