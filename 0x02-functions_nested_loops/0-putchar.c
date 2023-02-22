4794e182d45120011f39df97e70c4a62cdc91513
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-21 18:17:50 -0800

    functions0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,15 @@
 
 int main(void)
 {
-	_putchar('p'/n; 'u'/n; 't'/n; 'c'/n; 'h'/n; 'a'/n; 'r'/n);
-	putchar('/n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
