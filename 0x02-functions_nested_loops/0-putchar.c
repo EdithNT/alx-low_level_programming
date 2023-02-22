a0a37ecb8ab1f2fc6e8f8f3414cde55ebdde462a
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-21 17:38:32 -0800

    I am starting my work on functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main"
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char y = "_putchar";
+	int i;
+
+	for (i = 0; y = i; i++)
+	{
+	putchar(y);
+	}
+	putchar('\n')
+
+	return (0);
+}
