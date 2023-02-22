36a3cdc3765bb003d45f1880babe858579ce442b
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-21 18:10:29 -0800

    my function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,8 @@
 
 int main(void)
 {
-	char y[] = "_putchar";
-	int i;
-
-	for (i = 0; y[i]; i++)
-	putchar(y[i]);
-	_putchar('\n');
+	_putchar('p'/n; 'u'/n; 't'/n; 'c'/n; 'h'/n; 'a'/n; 'r'/n);
+	putchar('/n');
 
 	return (0);
 }
