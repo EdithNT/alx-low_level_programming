c9fc10cd9f66e89a81b67729ae9a2c0b6c50c6d8
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-22 09:52:55 -0800

    search for lowercase letters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -10,9 +10,8 @@
 
 int _islower(int c)
 {
-	for (c = 'a'; c <= 'z'; c++)
-		if (c >= 'a' && c <= 'z')
-	return (1);
-		else
-			return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
