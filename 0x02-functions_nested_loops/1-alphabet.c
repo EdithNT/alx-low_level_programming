a0545ffee0c86e7f4a21975aec5768445a791745
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-21 19:28:23 -0800

    lowalphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,13 @@
  * Return: 0
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char alphabet = 'a';
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-	_putchar(alphabet);
+		_putchar(alphabet);
 	}
 	_putchar('\n');
 
