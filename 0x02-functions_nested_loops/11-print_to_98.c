390bb5eb1acebcaf21cf08907bad5b7b750310d3
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-22 20:55:40 -0800

    natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - print natural numbers from n to 98
+ *
+ * @n: value of integer 
+ *
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
+		else
+			for (n = n; n > 98; n--)
+				printf("%d, ", n);
+		printf("98\n");
+}
