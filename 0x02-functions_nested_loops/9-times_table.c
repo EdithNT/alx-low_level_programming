60cfc5ccc9e35317ea70a098fee19de93dab5bda
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-22 19:52:07 -0800

    timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *times_table: print out the 9 times table
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int t, s, prod;
+
+	for (t = 0; t <= 9; t++)
+	{
+		_putchar('0');
+		for (s = 1; s <= 9; s++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = t * s;
+			if (prod <= 9)
+				_putchar(' ');
+
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}	
