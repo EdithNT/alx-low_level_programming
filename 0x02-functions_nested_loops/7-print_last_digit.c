9b50c979503c452990018e2082fe4b034ff6387d
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-22 17:05:22 -0800

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,6 +2,7 @@
 
 /**
  * print_last_digit - prints the last digit of the number
+ * @y: value of integer whose last digit we are finding
  *
  * Return: 0
  */
@@ -9,10 +10,10 @@
 int print_last_digit(int y)
 {
 	y = y % 10;
-	
+
 	if (y < 0)
 		y = -y;
 	_putchar(y + '0');
 
 	return (y);
-}	
+}
