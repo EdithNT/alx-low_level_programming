5d3362b005f17a96f64bbc4ed4c698d6c6cb0a75
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-22 17:09:43 -0800

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,14 @@
 
 /**
  * _abs - computes the absolute value of an integer
+ * @a: integer value whise abs value we are calculating
  *
  * Return: 0
  */
 
-int _abs(int)
+int _abs(int a)
 {
-	int a;
-	if (a <= o)
+	if (a <= 0)
 		return (a);
 	else
 		return (-a);
