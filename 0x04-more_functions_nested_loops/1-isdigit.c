8d54251d4ea30331ec117e1aa5556656cbee6710
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-23 20:34:25 -0800

    digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,14 +3,16 @@
 /**
  * _isdigit - checks for a digit in the code
  *
+ * @c: output value
  * Return: 1 if c is a digit, 0 otherwise
  */
 
 int _isdigit(int c);
 {
-	if (c = 0 && c = 9)
+	if (c = '0' && c = '9')
 	{
 		return (1);
 	}
+
 	return (0);
 }
