c17f638bcb443d86d196dca420b86ed645a4ecc2
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-23 20:01:12 -0800

    capital letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - we are checking for upper case letters
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+
+int _isupper(int c);
+{
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		return (1);
+	return (0);
+	}
+}
