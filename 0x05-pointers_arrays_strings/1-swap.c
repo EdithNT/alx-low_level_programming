3c5e795ce7e1b421b523e5eabf18be4a1701698b
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-27 14:58:05 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	temp = a;
+	a = b;
+	b = temp;
+}
