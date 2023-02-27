7afcd7a83653be8750f7fd6e6c29dc8d9a3d2d7e
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-27 13:58:01 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - take a pointer to an int as a parameter
+ * @*n: pointer to an integer
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	int *n = 98;
+
+	return (0);
+}
