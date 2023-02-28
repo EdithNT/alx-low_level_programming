0112091f8075a2052db3be5273d877f72527c91b
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-02-27 16:41:29 -0800

    task4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - print a string, followed by a newline, to stdout
+ * @str: value of string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	char *str;
+
+	while (*str)
+		_putchar(*s++);
+}
