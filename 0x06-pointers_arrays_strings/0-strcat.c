5d87595e38d3ecda1638329eb33eaf143282b9f2
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-01 16:24:53 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strcat - concatenates two strings
+ *@src: first string to append
+ *@dest: location to append the first string
+ *
+ *Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	while (dest[i] != '\0')
+		j++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[j++] = src[i];
+
+	return (dest);
+}
