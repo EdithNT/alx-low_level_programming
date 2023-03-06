035c322dac475f3425cbdc8b8514199728343a0c
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-06 09:02:29 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @b: constant byte
+ * @s: pointer to the memory area
+ * @n: number of bytes of the memory area
+ *
+ * Return: a pointer to the memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char s;
+
+	_memset('b', s);
+	_putchar('\n');
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+still more pointers, arrays and strings
