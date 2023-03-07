528b97bf37f3cfd3684ed2f1730901468f771dc7
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-06 16:55:08 -0800

    prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring 
+ * @s:
+ * @accept: 
+ *
+ * Return: the number of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int a = _strspn(s, accept);
+
+	if ( a == 0)
+	{
+		return (0);
+	}
+	return a;
+}
