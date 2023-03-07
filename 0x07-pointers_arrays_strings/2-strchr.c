f711f08fac22382676b666547054b24392cf8760
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-06 16:04:26 -0800

    memcpy

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: first character
+ * @c: second character
+ *
+ * Return: a pointer to the first occurrence of the character, or null if character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+	return (NULL);
+}
