074071fabd5318d976b649d5653ebc7b32858f0e
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-08 08:18:32 -0800

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a newline
+ * @s: the string to be printed
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	char c[];
+
+	_putchar("%s\n", c);
+}
