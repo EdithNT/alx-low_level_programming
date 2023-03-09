4d0409ac81fef4f9e2cf783c325fc0506051ec29
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-08 19:43:30 -0800

    power of recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to
+ * the power of y
+ * @x: base value
+ * @y: power to raise
+ *
+ * Return: -1 if y is lower than 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+		return (_pow_recursion++);
+}
