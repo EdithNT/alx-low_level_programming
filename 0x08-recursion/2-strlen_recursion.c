c77d9220947ea8fe428e390d0c7c938aa7b440a3
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-08 16:59:18 -0800

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: points to a string
+ *
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+		_strlen_recursion(*s);
+		return (0);
+}
