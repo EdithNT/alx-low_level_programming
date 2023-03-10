19b826e484f4ae67ce21af9c46a985c1160fb9c0
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-09 17:10:11 -0800

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+int pal_helper(char *s, int i, int j)
+{
+	if (s[i] == s[j])
+		return (1);
+	else
+		return (0);
+}
+
 /**
  * is_palindrome - checks to see if a string is a palindrome
  * @s: the string to be checked
@@ -7,10 +15,8 @@
  * Return: 1 if it is a palindrome, or 0 otherwise
  */
 
-int is_palindrome(char *s, int i, int j)
+int is_palindrome(char *s)
 {
-	if (s[i] == s[j])
-		return (1);
-	else
-		return (0);
+	is_palindrome(s);
+	return (0);
 }
