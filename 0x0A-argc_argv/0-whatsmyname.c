be235108004e9f72d068575421f15208ada7e026
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-10 19:22:43 -0800

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: first argument
+ * @argv: second argument to print an array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
