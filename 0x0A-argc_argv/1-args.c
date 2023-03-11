259fd147604470264d0c77a3839aae85e0178492
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-10 21:10:01 -0800

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: first argument
+ * @argv: second argument
+ *
+ * Return: 0
+ */
+
+int main(argc, argv[])
+{
+	printf("argc = %d\n", argc);
+
+	return (0);
+}
