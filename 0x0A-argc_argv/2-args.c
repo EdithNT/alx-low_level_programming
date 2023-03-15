d9730832334698d6a5d237551fd80ba43d05375d
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-15 12:46:28 -0700

    args2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - 
+ * @argc:
+ * @argv:
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", argv++;
+	return (0);
+}
