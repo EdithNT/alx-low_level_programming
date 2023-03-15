75d47a92c9d4bdde6980ee904d93164fb9266542
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-15 14:08:04 -0700

    args2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - starting point for running
@@ -14,13 +15,15 @@ int main(int argc, char *argv[])
 	int i, j, prod;
 
 	if (argc != 3)
+	{
 		printf("Error\n");
-	return (1);
+		return (1);
+	}
 
 	i = atoi(argv[1]);
 	j = atoi(argv[2]);
-
 	prod = i * j;
+
 	printf("%i\n", prod);
 
 	return (0);
