4f89479628084bc9cc79af7317b025725fcd3e65
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-15 14:02:37 -0700

    args2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - starting point for running
+ * @argc: count of arguments in the function
+ * @argv: array of pointers to argument strings
+ *
+ * Return: Error if the program doesn't receive two arguments,
+ * otherwise, 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, prod;
+
+	if (argc != 3)
+		printf("Error\n");
+	return (1);
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+
+	prod = i * j;
+	printf("%i\n", prod);
+
+	return (0);
+}
