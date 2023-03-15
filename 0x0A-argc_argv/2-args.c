a8492a98df327cbc8256d9f30ec2b9278db7ef7f
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-15 13:10:23 -0700

    args2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - entry point
  * @argc: count arguments
- * @argv: array of arguments
+ * @argv: array of argument pointers
  *
  * Return: 0
  */
@@ -11,7 +12,7 @@
 int main(int argc, char **argv[])
 {
 	while (argc--)
-		printf("%s\n", *argv++;
+		printf("%s\n", *argv++);
 
 	return (0);
 }
