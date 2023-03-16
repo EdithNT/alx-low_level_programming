4da19af3d819cb95fc4c3ecbf08fc7b31cf93c84
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-15 19:03:25 -0700

    sum

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,13 +14,12 @@ int main(int argc, char *argv[])
 
 	if (argc > 1)
 		for (i = 1; i < argc; i++)
-	{
-
-		for (j = 0; argv[i][j]; j++)
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-				return (printf("Error\n"), 1);
-		res += atoi(argv[i]);
-	}
+		{
+			for (j = 0; argv[i][j]; j++)
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+					return (printf("Error\n"), 1);
+			res += atoi(argv[i]);
+		}
 
 	printf("%i\n", res);
 	return (0);
