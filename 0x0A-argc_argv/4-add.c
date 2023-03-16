a2150b8480879d0259b4fa6c2d254dcaa265513f
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-15 17:49:40 -0700

    sum

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - starting point for running code
+ * @argc: count of arguments in the function
+ * @argv: array of pointers for function
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, sum;
+
+	if (argc != a || argc != b)
+	{
+		printf("0\n", argc);
+	}
+
+	if (a != a || b != b)
+		printf("Error\n", a, b);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	sum = a + b;
+
+	printf("%i\n", sum)
