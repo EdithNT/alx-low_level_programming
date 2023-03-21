2c2bfd66eba432cf7888891a9a5dd0b0c3475ec7
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-20 20:13:27 -0700

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,7 @@
  * @age: dog's age
  * @owner: dog's owner
  *
- * Description: a struct
+ * Description: no description
  */
 
 struct dog
@@ -16,4 +16,11 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};	
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
