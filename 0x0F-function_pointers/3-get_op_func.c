a0a7de12232cd2b1a00da0c63e56f692505295a7
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-24 19:12:33 -0700

    the goal will be reached

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,15 +19,11 @@ int (*get_op_func(char *s))(int, int)
 	{"/", op_div},
 	{"%", op_mod},
 	{NULL, NULL}
-};
+	};
 	int i;
 
-	if (s == NULL)
-		return (NULL);
-
-	while (ops[i].op == s)
-	{
-		return (*ops[i].p);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
+
+	return (ops[i].f);
 }
