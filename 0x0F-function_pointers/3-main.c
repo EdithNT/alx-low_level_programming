262f20ea9f84eb07bdc8ef16f2c79610242e52a2
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-24 19:37:05 -0700

    the goal will be attained

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,16 +20,19 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	f = get_op_func(argv[2]);
+
 	if (f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	c = f(a, b);
 	printf("%d\n", c);
+
 	return (0);
 }
-
