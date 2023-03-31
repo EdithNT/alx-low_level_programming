86e601884ca214d0de9a0e085b0ecf457817159e
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-30 22:06:58 -0700

    task 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to data of type list_t
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes;
+
+	while (h)
+	{
+		if (h == NULL)
+		printf("[0] (nil)");
+	else
+	{
+		printf("[%d] %s\n ", h->len, h->str);
+		nodes++;
+		h = h->next;
+	}
+	}
+
+	return (nodes);
+}
