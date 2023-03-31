fc94d46f4346a74d91c61306d35b719578c8d051
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-03-30 23:07:10 -0700

    task2

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: pointer to data of type list_t
+ *
+ * Return: number of elements in list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t cnt = 0;
+
+	while (h->len != NULL)
+	{
+		cnt++;
+		h = h->next;
+	}
+
+	return (cnt)
+}
