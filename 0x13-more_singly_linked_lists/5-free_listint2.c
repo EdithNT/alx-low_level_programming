5a8c78e2257277d5df4bc803e8c2a86eda1d72ce
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-04-04 08:27:54 -0700

    changes to code

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to head pointer
+ *
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *link;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		link = *head;
+		link = link->next;
+		free(link);
+	}
+	head = NULL;
+}
