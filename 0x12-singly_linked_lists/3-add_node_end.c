2a8063619b767ec2b37433cf97f2d011124da8d0
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-04-05 15:26:41 -0700

    add node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -31,7 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	endNode->next = NULL;
 
 	temp = *head; /* assign ptr head to temp */
-	while (temp->next != NULL)
+	while (temp->next == NULL)
 	{
 		temp->next = endNode;
 		endNode->next = NULL;
