c2d5f683410abfa074c2b1471f5d788e2379029f
Edith Nkrumah Turkson <Edithturkson13@gmail.com>
2023-04-05 12:19:55 -0700

    task8

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,10 +16,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	temp = head;
 	if (head == NULL)
 		return (NULL);
-	else if (d == index)
-		return (temp->n);
-	while (d != index && temp->next != NULL)
+	while (temp->next != NULL)
 	{
+		if (d == index)
 		index++;
 		temp = temp->next;
 	}
